b2c268aeccaed8221773b5151ade519fbba8e980
Harshvardhan Ojha <42267022+hv-ojha@users.noreply.github.com>
2019-11-25 00:41:53 +0530

    add JWT Tokens for API request

---
+++ src/main/java/com/cubereum/api/Service/JwtUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.cubereum.api.Service;
+
+import java.util.ArrayList;
+
+import com.cubereum.api.Entity.User;
+import com.cubereum.api.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                new ArrayList<>());
+    }
+}
